eacb04580c4c7e04f8ed4eea41d82d49624a42f7
T.J. Mazeika <t@mazeika.me>
2016-05-15 23:06:03 -0400

    Create main.go, http.go, and lan.go

---
+++ controlifx/lan.go
@@ -0,0 +1,25 @@
+package controlifx
+
+type LanMessage struct {
+
+}
+
+type LanHeader struct {
+
+}
+
+type LanHeaderFrame struct {
+
+}
+
+type LanHeaderFrameAddress struct {
+
+}
+
+type LanHeaderProtocolHeader struct {
+
+}
+
+type LanHeaderPayload struct {
+
+}
